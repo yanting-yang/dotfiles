47c8cc4845eed49e420d48dca09974b315bded33
Yanting Yang <yantingyang2024@gmail.com>
2026-04-22 17:09:20 -0400

    Sync changes

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,3 +1,37 @@
--- bootstrap lazy.nvim, LazyVim and your plugins
-require("config.options")
-require("config.lazy")
+vim.opt.cursorline = true
+vim.opt.expandtab = true
+vim.opt.list = true
+vim.opt.number = true
+vim.opt.scrolloff = 5
+vim.opt.shiftwidth = 4
+vim.opt.signcolumn = 'yes'
+vim.opt.tabstop = 4
+vim.opt.relativenumber = true
+
+vim.pack.add({
+    {
+        src = 'https://github.com/nvim-neo-tree/neo-tree.nvim',
+        version = vim.version.range('3')
+    },
+    -- dependencies
+    "https://github.com/nvim-lua/plenary.nvim",
+    "https://github.com/MunifTanjim/nui.nvim",
+    -- optional, but recommended
+    "https://github.com/nvim-tree/nvim-web-devicons",
+})
+require('neo-tree').setup({
+    filesystem = {
+        filtered_items = { visible = true }
+    }
+})
+
+vim.pack.add({
+    'https://github.com/nvim-tree/nvim-web-devicons',
+    'https://github.com/nvim-lualine/lualine.nvim'
+})
+require('lualine').setup()
+
+vim.pack.add({
+    'https://github.com/akinsho/bufferline.nvim'
+})
+require("bufferline").setup{}
