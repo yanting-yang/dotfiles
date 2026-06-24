192be91ae47e0e8cbba45d2fbff8818a5a21de74
Yanting Yang <yantingyang2024@gmail.com>
2026-06-24 13:34:19 -0700

    nvim: use double quotes and tabs consistently

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -4,34 +4,34 @@ vim.opt.list = true
 vim.opt.number = true
 vim.opt.scrolloff = 5
 vim.opt.shiftwidth = 4
-vim.opt.signcolumn = 'yes'
+vim.opt.signcolumn = "yes"
 vim.opt.tabstop = 4
 vim.opt.relativenumber = true
 
 vim.pack.add({
-    {
-        src = 'https://github.com/nvim-neo-tree/neo-tree.nvim',
-        version = vim.version.range('3')
-    },
-    -- dependencies
-    "https://github.com/nvim-lua/plenary.nvim",
-    "https://github.com/MunifTanjim/nui.nvim",
-    -- optional, but recommended
-    "https://github.com/nvim-tree/nvim-web-devicons",
+	{
+		src = "https://github.com/nvim-neo-tree/neo-tree.nvim",
+		version = vim.version.range("3"),
+	},
+	-- dependencies
+	"https://github.com/nvim-lua/plenary.nvim",
+	"https://github.com/MunifTanjim/nui.nvim",
+	-- optional, but recommended
+	"https://github.com/nvim-tree/nvim-web-devicons",
 })
-require('neo-tree').setup({
-    filesystem = {
-        filtered_items = { visible = true }
-    }
+require("neo-tree").setup({
+	filesystem = {
+		filtered_items = { visible = true },
+	},
 })
 
 vim.pack.add({
-    'https://github.com/nvim-tree/nvim-web-devicons',
-    'https://github.com/nvim-lualine/lualine.nvim'
+	"https://github.com/nvim-tree/nvim-web-devicons",
+	"https://github.com/nvim-lualine/lualine.nvim",
 })
-require('lualine').setup()
+require("lualine").setup()
 
 vim.pack.add({
-    'https://github.com/akinsho/bufferline.nvim'
+	"https://github.com/akinsho/bufferline.nvim",
 })
-require("bufferline").setup{}
+require("bufferline").setup({})
