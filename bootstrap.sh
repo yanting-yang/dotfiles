7e3133587e81b04ac571c8340a27741ca76541e7
Yanting Yang <yantingyang2024@gmail.com>
2026-06-25 14:16:42 -0700

    Create .ssh dir before symlinking config

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,6 +2,7 @@
 cd "$(dirname "${BASH_SOURCE}")"
 rm "$HOME/.profile"; ln -s "$PWD/.profile" "$HOME/.profile"
 rm "$HOME/.bash"*; ln -s "$PWD/.bashrc" "$HOME/.bashrc"
+mkdir -p "$HOME/.ssh"
 rm "$HOME/.ssh/config"; ln -s "$PWD/.ssh/config" "$HOME/.ssh/config"
 mkdir -p "$HOME/.config"
 rm -rf "$HOME/.config/git"; ln -s "$PWD/.config/git" "$HOME/.config/git"
